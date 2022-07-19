8d92c885ddf7cd6122632ebb013be25f21c7e350
Vijay Sonawane <vjsonawane8530@gmail.com>
2022-07-19 06:57:30 +0530

    3commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: waitress-serve --port=8000 myblog.wsgi:application
+web: waitress-serve --port=$PORT myblog.wsgi:application
--- myblog/settings.py
+++ myblog/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = os.environ.get('SECRET_KEY', 'default_value')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['vjsblog.herokuapp.com', 'localhost']
 
 # Application definition
 
